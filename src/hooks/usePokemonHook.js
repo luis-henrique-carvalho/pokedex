bf8e534e3d30c70617200e0f604b707cf20ed657
luis-henrique-carvalho <luisdev9225@gmail.com>
2023-11-13 21:45:12 -0300

    :sparkles: feat: Add usePokemonHook for fetching Pokemon data

---
+++ src/hooks/usePokemonHook.js
@@ -0,0 +1,59 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const usePokemonHook = () => {
+  const [page, setPage] = useState(0);
+  const [pokemonData, setPokemonData] = useState([]); // [1,2,3,4,5,6,7,8,9,10
+  const [pokemon, setPokemon] = useState()
+  const { id } = useParams()
+
+  const getPokemonById = async (id) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await response.json()
+    setPokemon(data)
+  }
+
+  const fetchPokemonsList = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${page * 20}`);
+    const data = await response.json();
+    return data.results;
+  }
+
+  const fetchPokemon = async (url) => {
+    const pokemon = await fetch(url)
+    const data = await pokemon.json();
+    return data;
+  }
+
+  const fetchPokemonData = async () => {
+    try {
+      // Primeira requisição
+      const pokemonList = await fetchPokemonsList();
+
+      const pokemonDataPromises = pokemonList.map((pokemon) =>
+        fetchPokemon(pokemon.url)
+      );
+
+      const pokemonData = await Promise.all(pokemonDataPromises);
+
+      setPokemonData(pokemonData);
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemonData()
+  }, [page])
+
+  useEffect(() => {
+    getPokemonById(id)
+  }, [id])
+
+  return { page, setPage, pokemonData, pokemon }
+
+}
+
+export default usePokemonHook;
